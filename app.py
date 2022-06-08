2fa98b7d470d8e28c78fa8c829b94cee962a54ba
isabelafr696 <105250155+isabelafr696@users.noreply.github.com>
2022-06-08 01:17:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 import cloudpickle
+from flask import Flask, render_template, request
 
 with open('model.pkl', 'rb') as file_in:
-  model_loaded = cloudpickle.load(file_in)
+  model = cloudpickle.load(file_in)
 
-frase = input('Digite uma frase: ')
+app = Flask(__name__)
 
-if model_loaded.predict([frase])[0] == 0:
-    print('Comentário negativo')
-else:
-    print('Comentário positivo')
+@app.route('/')
+def homepage():
+  return render_template('homepage.html', nome='Fulano')
+
+@app.route('/predicao', methods=['POST'])
+def predicao():
+  Gender = int(request.form['Gender'])
+  Senior_Citizen = int(request.form['Senior_Citizen'])
+  Phone_Service = int(request.form['Phone_Service'])
+  Tech_Support = int(request.form['Tech_Support'])
+  Streaming_Movies = int(request.form['Streaming_Movies'])
+  Contract = int(request.form['Contract'])
+  Payment_Method = int(request.form['Payment_Method'])
+  predicao = model.predict([[Gender, Senior_Citizen, Phone_Service, Tech_Support, Streaming_Movies, Contract, Payment_Method]])
+  return render_template('predicao.html', predicao=predicao[0])
+  
+  
+app.run(debug=True)
